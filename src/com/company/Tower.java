a0144cc5d1d9ed4d30f99260be14fafea5aa27b8
ElieDahrouj <edarouj@hotmail.com>
2021-02-04 17:33:23 +0100

    init Tower class with constructor

--- src/com/company/Tower.java
+++ src/com/company/Tower.java
@@ -1,63 +1,38 @@
 package com.company;
-
 import java.util.*;
+import javafx.scene.image.Image;
 
 /**
  * 
  */
 public class Tower {
 
-    /**
-     * Default constructor
-     */
+    public int AD;
+    public int worth;
+    public int range;
+    public int AS;
+    public Image image;
+
     public Tower() {
+        this.AD = 25;
+        this.worth = 50;
+        this.AS = 1;
+        this.range = 200;
+        this.image = new Image("tiles/towerDefense_tile249.png");
     }
 
-    /**
-     * 
-     */
-    public int AD;
-
-    /**
-     * 
-     */
-    public int worth;
-
-    /**
-     * 
-     */
-    public int range;
-
-    /**
-     * 
-     */
-    public int AS;
-
-
-    /**
-     * 
-     */
     public void attack() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void upgrade() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void sell() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void buy() {
         // TODO implement here
     }
