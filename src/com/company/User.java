6953b5389d45fbf37e62531ca0b1c69762b2c6e3
ElieDahrouj <edarouj@hotmail.com>
2021-02-11 15:30:22 +0100

    generate money and stylize data display

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import javafx.css.FontFace;
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+
+import java.awt.*;
 
 /**
  *
@@ -12,10 +16,11 @@ public class User {
 
     public int hp;
     public String pseudo;
+    private int money;
     /**
      *
      */
-    private int money;
+    private Text moneyText;
     private Text hpText;
     /**
      *
@@ -30,8 +35,12 @@ public class User {
         this.setPane(pane);
         this.setHp(hp);
         this.setPseudo(pseudo);
+        this.setMoney(100);
         this.createUserInterface(pane, hp, pseudo);
     }
+    public void setMoneyText(Text moneyText) { this.moneyText = moneyText; }
+
+    public Text getMoneyText() { return moneyText; }
 
     public int getHp() {
         return hp;
@@ -52,18 +61,39 @@ public class User {
     private void createUserInterface(BorderPane pane, int hp, String pseudo) {
         Group root = new Group();
 
-        Text hpTextText = this.createText(String.valueOf(hp), 50, 70);
-        Text pseudoTextText = this.createText(pseudo, 50, 50);
+        Text hpTextText = this.createText("Hp: "+String.valueOf(hp), 50, 70);
+        Text pseudoTextText = this.createText(upperCaseFirst(pseudo), 50, 50);
 
+        Text userMoney = this.createText(String.valueOf("Money: " +this.getMoney()),50,90);
+
+        hpTextText.setFill(Color.RED);
+        userMoney.setFill(Color.GOLD);
+
+        this.setMoneyText(userMoney);
         this.setHpText(hpTextText);
         this.setPseudoText(pseudoTextText);
 
         root.getChildren().add(pseudoTextText);
         root.getChildren().add(hpTextText);
-
+        root.getChildren().add(userMoney);
         pane.getChildren().add(root);
 
         this.setPane(pane);
+    }
+
+    public static String upperCaseFirst(String val) {
+        char[] arr = val.toCharArray();
+        arr[0] = Character.toUpperCase(arr[0]);
+        return new String(arr);
+    }
+
+    public void buyTower(int price)
+    {
+        int newMoney = this.getMoney() - price;
+        this.setMoney(newMoney);
+        this.getMoneyText().setText("Money: "+String.valueOf(newMoney));
+
+        System.out.println(this.getMoney());
     }
 
     private Text createText(String sentence, int X, int Y) {
@@ -111,7 +141,7 @@ public class User {
     public boolean receiveDmg(int dmg) {
         int hp = this.getHp() - dmg;
         this.setHp(hp);
-        this.getHpText().setText(String.valueOf(hp));
+        this.getHpText().setText("Hp: "+String.valueOf(hp));
 
         if (hp <= 0) {
             Text GameOver = this.createText("GAME OVER", 1000, 600);
